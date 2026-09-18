9e7172fd58873c2f5e98b71554fc2dfcee828f2b
jvvppereira <joao.vitor.paes@hotmail.com>
2026-09-17 21:32:29 -0300

    chore: remove unused mongoose import from index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
 const { connectDB } = require('./config/db');
 const { addClient, removeClient } = require('./utils/sseManager');
